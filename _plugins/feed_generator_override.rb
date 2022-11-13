55c56cfbad114c19968f48151b15dd500dec166e
chris0v <117482707+chris0v@users.noreply.github.com>
2022-11-13 19:08:33 +0100

    Under construction

---
+++ _plugins/feed_generator_override.rb
@@ -0,0 +1,11 @@
+module JekyllFeed
+    class Generator < Jekyll::Generator
+      safe true
+      priority :lowest
+  
+      # Main plugin action, called by Jekyll-core
+      def generate(_site)
+      end
+    end
+  end
+  
--- index.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: default
-title: Test Title
----
-Hello World!
-
-This is a KaTeX test.
-
-$$E = mc^2$$
